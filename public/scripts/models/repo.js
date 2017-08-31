c2b29212161451a0bb57d01c786e67b4f7b48f9f
toastynerd <tylermorgan86@gmail.com>
2017-08-30 21:25:03 -0700

    fix repos typo

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -18,10 +18,10 @@ var app = app || {};
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
-  repos.with = function(attr){
+  repos.with = function(attr) {
     return repos.all.filter(function(repo) {
-      return repo[attr]);
-    }
+      return repo[attr];
+    })
   }
 
   module.repos = repos;
